3a3f68022819b3c367dcd0844bc6aeb6b06fe4a3
Yangpluseven <yangpluseven@163.com>
2023-12-25 01:09:37 +0800

    Well ... Changed a lot.

--- simulation/src/constants/Constants.java
+++ simulation/src/constants/Constants.java
@@ -1,5 +1,7 @@
 package constants;
 
+import entities.Size;
+
 /**
  * Includes some of the global constants. From a certain perspective, this class
  * should be eliminated, and the constants here should be added to their
@@ -10,10 +12,8 @@ package constants;
  */
 public class Constants {
 
-	public static final int INIT_GRID_WIDTH = 5;
-	public static final int INIT_GRID_HEIGHT = 5;
-	public static final int NUM_COLS = 100;
-	public static final int NUM_ROWS = 60;
+	public static final Size INIT_GRID_SIZE = new Size(5, 5);
+	public static final Size NUM_OF_COL_ROW = new Size(100, 60);
 	public static final int BACKGROUND_Z_ORDER = 0;
 	public static final int TERRAIN_BOTTOM_Z_ORDER = 1;
 	public static final int CREATURE_BOTTOM_Z_ORDER = 6;
--- simulation/src/test/GUITest.java
+++ simulation/src/test/GUITest.java
@@ -1,9 +1,12 @@
 package test;
 
-import simulator.SimulatorView;
-import entities.Location;
-import entities.View;
-
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 /**
@@ -16,13 +19,55 @@ public class GUITest {
 	public static void main(String[] args) {
 
 		SwingUtilities.invokeLater(() -> {
-			SimulatorView simuView = new SimulatorView();
-			Location location = new Location(99, 59);
-			View view = new View(simuView);
-			view.setLocation(location);
-			simuView.addView(view);
-//			view.delete(simuView);
+			F f = new F();
+			f.p.prepare();
+			f.p.draw(0, 0, Color.BLUE);
+			f.p.draw(10, 10, Color.BLACK);
 		});
+	}
+
+}
+
+class F extends JFrame {
+	
+	public P p;
+	
+	public F() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		p = new P();
+		add(p, BorderLayout.CENTER);
+		pack();
+		setVisible(true);
+		p.repaint();
+	}
+	
+}
+
+class P extends JPanel {
+
+	public Image i;
+	public Graphics g;
+
+	public P() {
+		setPreferredSize(new Dimension(100, 60));
+	}
+	
+	public Dimension getPreferredSize() {
+		return new Dimension(100, 60);
+	}
+	
+	public void prepare() {
+		i = createImage(100, 60);
+		g = i.getGraphics();
+	}
+	
+	public void draw(int x, int y, Color color) {
+		g.setColor(color);
+		g.fillRect(x, y, 10, 10);
+	}
+
+	public void paintComponent(Graphics g) {
+		g.drawImage(i, 0, 0, null);
 	}
 
 }
